cf25f01aba0e772e26113d469f257c79aab7e7a6
Alexander Konovalov <alex.knvl@gmail.com>
2016-11-27 15:47:26 -0500

    Removed Mutable, added Handle.

--- core/src/main/scala/sio/core/Ref.scala
+++ core/src/main/scala/sio/core/Ref.scala
@@ -1,9 +1,11 @@
 package sio.core
 
-final class Ref[S, A] private[core] (var unsafeValue: A) extends Mutable[S, A] {
-  def read: ST[S, A] = capture { unsafeValue }
-  def write(a: => A): ST[S, Unit] = capture { unsafeValue = a }
-  def modify(f: A => A): ST[S, A] = capture { unsafeValue = f(unsafeValue); unsafeValue }
+final class Ref[S, A] private[core] (var unsafeValue: A) {
+  def id: String = super.toString.dropWhile(_ != '@').tail
 
-  override def toString: String = s"Ref[$id]"
+  def read: ST[S, A] = ST.unsafeCapture { unsafeValue }
+  def write(a: => A): ST[S, Unit] = ST.unsafeCapture { unsafeValue = a }
+  def modify(f: A => A): ST[S, A] = ST.unsafeCapture { unsafeValue = f(unsafeValue); unsafeValue }
+
+  override def toString: String = s"Ref[${unsafeValue.getClass.getSimpleName}]@$id"
 }
--- example/src/main/scala/st.scala
+++ example/src/main/scala/st.scala
@@ -4,12 +4,15 @@ import scala.reflect.ClassTag
 import sio.teletype.putStrLn
 
 object st {
-  final class JArray[S, A](value: Array[A]) extends Mutable[S, Array[A]] {
-    def length: ST[S, Int] = capture { value.length }
-    def get(i: Int): ST[S, A] = capture { value(i) }
-    def set(i: Int, x: A): ST[S, Unit] = capture { value(i) = x }
-    def map(f: A => A): ST[S, Unit] = capture { for (i <- value.indices) value(i) = f(value(i)) }
+  type JArray[S, E] = Handle[S, Array[E]]
+
+  implicit class ArraySyntax[S, E](handle: JArray[S, E]) extends Handle.Syntax(handle) {
+    def length: ST[S, Int] = lift { _.length }
+    def get(i: Int): ST[S, E] = lift { _.apply(i) }
+    def set(i: Int, x: E): ST[S, Unit] = lift { _.update(i, x) }
+    def map(f: E => E): ST[S, Unit] = lift { a => for (i <- a.indices) a(i) = f(a(i)) }
   }
+
   object JArray {
     def fill[S, A](len: Int)(value: => A)(implicit A: ClassTag[A]): ST[S, JArray[S, A]] =
       ST.unsafeCapture { new JArray[S, A](Array.fill(len)(value)) }
