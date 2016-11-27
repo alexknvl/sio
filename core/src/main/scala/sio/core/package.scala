7b49c21d2a0b34b08c1ae58422cf9111f1bbf3da
Alexander Konovalov <alex.knvl@gmail.com>
2016-11-27 16:27:10 -0500

    Added STArray.

--- core/src/main/scala/sio/core/package.scala
+++ core/src/main/scala/sio/core/package.scala
@@ -1,10 +1,15 @@
 package sio
 
+import java.util
+import java.util.Comparator
 import java.util.concurrent.Callable
 
+import cats.kernel.Order
 import sio.core.control.{RunInBase, MonadControlIO}
 
-import scala.util.Try
+import scala.annotation.tailrec
+import scala.reflect.ClassTag
+import scala.util.{Sorting, Try}
 import cats.syntax.all._
 
 package object core {
@@ -40,4 +45,31 @@ package object core {
 
   def newSTRef[S, A](a: A): ST[S, Ref[S, A]] = ST.unsafeCapture { new Ref[S, A](a) }
   def newIORef[A](a: => A): IO[IORef[A]] = IO { new Ref[RealWorld, A](a) }
+
+  type STArray[S, E] = Handle[S, Array[E]]
+  implicit class STArraySyntax[S, E](handle: STArray[S, E]) extends Handle.Syntax(handle) {
+    def length: ST[S, Int] = lift { _.length }
+
+    def get(i: Int): ST[S, E] = lift { _.apply(i) }
+    def set(i: Int, x: E): ST[S, Unit] = lift { _.update(i, x) }
+
+    def transform(f: E => E): ST[S, Unit] = lift { a =>
+      @tailrec def go(i: Int): Unit =
+        if (i < a.length) {
+          a.update(i, f(a(i)))
+          go(i + 1)
+        } else ()
+      go(0)
+    }
+
+    def stableSort(implicit E: Order[E], CT: ClassTag[E]): ST[S, Unit] =
+      lift { a => Sorting.stableSort(a)(CT, E.toOrdering) }
+    def quickSort(implicit E: Order[E], CT: ClassTag[E]): ST[S, Unit] =
+      lift { a => Sorting.quickSort(a)(E.toOrdering) }
+  }
+
+  object STArray {
+    def fill[S, A](len: Int)(value: => A)(implicit A: ClassTag[A]): ST[S, STArray[S, A]] =
+      ST.unsafeCapture { new STArray[S, A](Array.fill(len)(value)) }
+  }
 }
--- example/src/main/scala/st.scala
+++ example/src/main/scala/st.scala
@@ -1,33 +1,20 @@
 import sio.core._
 
-import scala.reflect.ClassTag
 import sio.teletype.putStrLn
+import cats.kernel.instances.int._
 
 object st {
-  type JArray[S, E] = Handle[S, Array[E]]
-
-  implicit class ArraySyntax[S, E](handle: JArray[S, E]) extends Handle.Syntax(handle) {
-    def length: ST[S, Int] = lift { _.length }
-    def get(i: Int): ST[S, E] = lift { _.apply(i) }
-    def set(i: Int, x: E): ST[S, Unit] = lift { _.update(i, x) }
-    def map(f: E => E): ST[S, Unit] = lift { a => for (i <- a.indices) a(i) = f(a(i)) }
-  }
-
-  object JArray {
-    def fill[S, A](len: Int)(value: => A)(implicit A: ClassTag[A]): ST[S, JArray[S, A]] =
-      ST.unsafeCapture { new JArray[S, A](Array.fill(len)(value)) }
-  }
-
-  def calculate[S]: ST[S, Int] = for {
-    a <- JArray.fill(10)(2)
-    _ <- a.map(_ * 2)
+  def calculate[S]: ST[S, (Int, Int)] = for {
+    a <- STArray.fill(10)(2)
+    _ <- a.transform(_ * 2)
     _ <- a.set(3, 0)
-    x <- a.get(3)
-    y <- a.get(1)
-  } yield x + y
+    _ <- a.stableSort
+    first <- a.get(0)
+    second <- a.get(1)
+  } yield (first, second)
 
   def run: IO[Unit] = {
-    val x = ST.attempt(new Forall[ST[?, Int]] { def apply[A]: ST[A, Int] = calculate })
+    val x = ST.attempt(new Forall[ST[?, (Int, Int)]] { def apply[A]: ST[A, (Int, Int)] = calculate })
     putStrLn(x.toString)
   }
 }
