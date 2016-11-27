2e10f74ce850f6c5aed54cb4ce6aef0ba2071944
Alexander Konovalov <alex.knvl@gmail.com>
2016-11-27 14:03:34 -0500

    Better Mutable.toString

--- core/src/main/scala/sio/core/Mutable.scala
+++ core/src/main/scala/sio/core/Mutable.scala
@@ -6,6 +6,6 @@ abstract class Mutable[S, A] {
   protected def capture[F](f: => F): ST[S, F] = ST { RealIO.capture(f) }
 
   def id: String = super.toString.dropWhile(_ != '@').tail
-  override def toString: String = s"STMutable[$id]"
+  override def toString: String = s"${this.getClass.getSimpleName}[$id]"
 }
 
