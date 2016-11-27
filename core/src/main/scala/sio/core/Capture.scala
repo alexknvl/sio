c990f25ea041ad1e8b7bb70be90f0d66b331d9b7
Alexander Konovalov <alex.knvl@gmail.com>
2016-11-27 07:16:38 -0500

    Removed Capture.

--- core/src/main/scala/sio/core/Capture.scala
+++
@@ -1,7 +0,0 @@
-package sio.core
-
-import simulacrum.typeclass
-
-@typeclass trait Capture[F[_]] {
-  def capture[A](f: => A): F[A]
-}
--- core/src/main/scala/sio/core/LiftIO.scala
+++ core/src/main/scala/sio/core/LiftIO.scala
@@ -6,9 +6,9 @@ import cats.syntax.functor._
 import cats.kernel.Monoid
 import simulacrum.typeclass
 
-@typeclass trait LiftIO[F[_]] extends Capture[F] {
-  def capture[A](a: => A): F[A] = liftIO(IO(a))
+@typeclass trait LiftIO[F[_]] {
   def liftIO[A](a: IO[A]): F[A]
+  def capture[A](a: => A): F[A] = liftIO(IO(a))
 }
 
 object LiftIO {
